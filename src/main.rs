0da22e5584150046b0a2c706d97d529b99ebf382
remy <remyciterin@gmail.com>
2023-01-16 00:54:17 +0100

    new feature: generate a new hmap considering the shape of the toolgit add ./

--- src/main.rs
+++ src/main.rs
@@ -9,25 +9,14 @@ pub fn main() {
         Rgb::from_slice(&[255, 255, 255]).clone()
     ).unwrap();
 
-    println!("Hello, world!");
+    println!("Hello, world! {}", hmap1.get(0, 0));
 
-    /*let mut hmap2 = HeightMap::new(20, 20);
-    for i in 0..hmap2.get_width() {for j in 0..hmap2.get_height() {
-        hmap2.set(i, j, 1.4 * (i as f64) + 0.73 * (j as f64));
-    }}
 
-    let out = hmap1.par_get_max_plus_convolve(&hmap2);
+    let depth = 0.002;
 
-    assert_eq!(out.get_height(), hmap1.get_height() + 1 - hmap2.get_height());
-    assert_eq!(out.get_width(), hmap1.get_width() + 1 - hmap2.get_width());
+    // adapt the hmap with the shape of the tool for under-approximate the final shape
+    let hmap2 = hmap1.generate_tool_hmap(0.2, 0.1, depth, ToolShape::Flat(0.0012));
 
-    let mut result = 0.0;
-    for i in 0..out.get_width() {
-        for j in 0..out.get_height() {
-            result += out.get(i, j);
-        }
-    }*/
-
-    hmap1.save(-1.0, 0.0, "test_out.png").expect("unable to save the image");
+    hmap2.save(-2.0 * depth, 1.0 * depth, "test_out.png").expect("unable to save the image");
     //println!("{}", result);
 }
